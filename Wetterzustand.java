5be5a96b1e45a29d022ee070b8df6bd348ca5244
Schmallex <i_lole@web.de>
2017-05-16 11:28:11 +0200

    a05

---
+++ Wetterzustand.java
@@ -0,0 +1,16 @@
+package a05;
+
+
+public class Wetterzustand{
+	final String sonne="SONNIG";
+	final String bedeckt ="BEDECKT";
+	final String wolkig = "WOLKIG";
+	final String regen ="REGEN";
+	final String gewitter ="GEWITTER";
+	
+
+}
+
+
+
+	
