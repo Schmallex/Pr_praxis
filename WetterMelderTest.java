226819e618058673fa6c1292f2a72f079c91b583
Schmallex <i_lole@web.de>
2017-05-22 17:56:48 +0200

    Update WetterMelderTest.java

--- WetterMelderTest.java
+++ WetterMelderTest.java
@@ -13,38 +13,36 @@ public class WetterMelderTest {
         DirtyPainter paint = new DirtyPainter();
         WeatherSymbol symbol = new WeatherSymbol(50, 50);
         WetterMelder melder = new WetterMelder(paint, symbol);
+        WetterPrinter print = new WetterPrinter();
 
-        Timer sonne = new Timer(50);
+        Timer sonne = new Timer(9000);
         sonne.addActionListener(melder);
-        sonne.setDelay(9000);
+        sonne.addActionListener(print);
         sonne.setActionCommand(w.sonne);
 
-        Timer wolkig = new Timer(25);
+        Timer wolkig = new Timer(12000);
         wolkig.addActionListener(melder);
-        wolkig.setDelay(12000);
+        wolkig.addActionListener(print);
         wolkig.setActionCommand(w.wolkig);
 
-        Timer regen = new Timer(14);
+        Timer regen = new Timer(15000);
         regen.addActionListener(melder);
-        regen.setDelay(15000);
+        regen.addActionListener(print);
         regen.setActionCommand(w.regen);
 
-        Timer bedeckt = new Timer(10);
+        Timer bedeckt = new Timer(17000);
         bedeckt.addActionListener(melder);
-        bedeckt.setDelay(17000);
+        bedeckt.addActionListener(print);
         bedeckt.setActionCommand(w.bedeckt);
 
-        Timer gewitter = new Timer(18);
+        Timer gewitter = new Timer(20000);
         gewitter.addActionListener(melder);
-        gewitter.setDelay(20000);
+        gewitter.addActionListener(print);
         gewitter.setActionCommand(w.gewitter);
-        while (true) {
-            sonne.start();
-            gewitter.start();
-            bedeckt.start();
-            regen.start();
-            wolkig.start();
-        }
-
+        sonne.start();
+        gewitter.start();
+        bedeckt.start();
+        regen.start();
+        wolkig.start();
     }
 }
