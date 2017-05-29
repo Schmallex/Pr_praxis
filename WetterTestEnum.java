b2ef467276bee9b9e0e048c64308c5bac533f3a7
Schmallex <i_lole@web.de>
2017-05-29 20:25:37 +0200

    Add files via upload

---
+++ WetterTestEnum.java
@@ -0,0 +1,56 @@
+package a07.a05;
+
+import a05.WetterPrinter;
+import schimkat.berlin.lernhilfe2017ss.event.Timer;
+
+/**
+ * Created by Alex on 23.05.2017.
+ */
+public class WetterTestEnum {
+    public static void main(String[] args) {
+
+        WetterAnsicht ansicht1 = new WetterAnsicht();
+        WetterAnsicht ansicht2 = new WetterAnsicht();
+
+        WetterModel model1 = new WetterModel(ansicht1);
+        WetterModel model2 = new WetterModel(ansicht2);
+
+        WetterEmpfangsZentrale w2= new WetterEmpfangsZentrale(model2);
+        WetterEmpfangsZentrale w = new WetterEmpfangsZentrale(model1);
+
+        WetterPrinter print =new WetterPrinter();
+
+        Timer sonne = new Timer(9000);
+        sonne.setActionCommand(WetterZustand.SONNIG.name());
+        sonne.addActionListener(w);
+        sonne.addActionListener(print);
+
+        Timer regen = new Timer(15000);
+        regen.setActionCommand(WetterZustand.REGEN.name());
+        regen.addActionListener(w);
+        regen.addActionListener(w2);
+        regen.addActionListener(print);
+
+        Timer wolkig = new Timer(12000);
+        wolkig.addActionListener(w);
+        wolkig.addActionListener(print);
+        wolkig.setActionCommand(WetterZustand.WOLKIG.name());
+
+        Timer bedeckt = new Timer(17000);
+        bedeckt.addActionListener(w);
+        bedeckt.addActionListener(w2);
+        bedeckt.addActionListener(print);
+        bedeckt.setActionCommand(WetterZustand.BEDECKT.name());
+
+        Timer gewitter = new Timer(20000);
+        gewitter.addActionListener(w);
+        gewitter.addActionListener(print);
+        gewitter.setActionCommand(WetterZustand.GEWITTER.name());
+
+        gewitter.start();
+        bedeckt.start();
+        regen.start();
+        wolkig.start();
+        sonne.start();
+    }
+}
